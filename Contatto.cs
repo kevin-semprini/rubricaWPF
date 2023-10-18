d21956f09df7dd23734acac81f3f45fec0c0cd7c
Semprini Kevin <120457816+kevin-semprini@users.noreply.github.com>
2023-10-18 10:18:28 +0200

    Add files via upload

---
+++ Contatto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kevin.Semprini._4i.Rubrica
+{
+    internal class Contatto
+    {
+        private int _numero = 0;
+        private string _cognome = "";
+
+        public int Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new ArgumentOutOfRangeException();
+
+                _numero = value;
+            }
+        }
+
+        public string Nome { get; set; }
+        public string EMail { get; set; }
+        public string Telefono { get; set; }
+        public string Citta { get; set; }
+        public string CAP { get; set; }
+
+        public string Cognome { get => _cognome; set => _cognome = value; }
+
+        public Contatto() { }
+
+        //COSTRUISCE IL CONTATTO PARTENDO DA UNA RIGA DEL FILE CSV
+        public Contatto(string riga) 
+        {
+            // ".split()" crea una vettore di elemnti stringa
+            string[] campi = riga.Split(';');
+            if (campi.Length >= 5)
+            {
+                this.Nome = campi[0];
+                this.Cognome = campi[1];
+                this.Telefono = campi[2];
+                this.EMail = campi[3];
+                this.CAP = campi[4];
+            }
+
+             
+            
+
+            if (this.Telefono == " ")
+            {
+                throw new ArgumentException();
+            }
+
+
+
+        }
+        public Contatto(int numero, string nome, string cognome)
+        {
+            Numero = numero;
+            Nome = nome;
+            Cognome = cognome;
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Kevin.Semprini._4i.Rubrica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+            
+            Contatto[] Contatti = new Contatto[100];
+            for (int i = 0; i < Contatti.Length; i++)
+            {
+                Contatti[i] = new Contatto();
+            }
+
+
+            try
+            {
+                StreamReader fin = new StreamReader("Dati.csv"); //legge i dati dal file "Dati.csv"
+                string riga;
+                int idx = 0;
+                while (!(fin.EndOfStream))
+                {
+                    riga = fin.ReadLine();
+                    Contatto c = new Contatto(riga);
+                    // l'indice "idx++" serve per far scrivere dentro il valore di idx per poi incrementarlo senza scrivere nel secondo valore
+                    //si chiama "post-incremento"
+                    Contatti[idx++] = c;
+                }
+
+            } catch (Exception err) 
+            {
+                MessageBox.Show("NUH HUH\n" + err.Message);
+            }
+
+            dgDati.ItemsSource = Contatti;
+
+        }
+    }
+}
